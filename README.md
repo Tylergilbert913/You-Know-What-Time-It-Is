5aa52da20eececf4ecc1ced006395a7efaa4b486
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-01-23 13:58:37 -0500

    Setup my html and added in the css

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # You-Know-What-Time-It-Is
+
+// on click listener for the save button
+    // save values of text to local storage
+    // values will be from the textarea
+    // function that uses moment.js and updates each color block in textFill to represent current time
+    // load any saved data from local storage to correct div
+    // display current day at top of page
+    // $("#current-day").text([PUT MOMEMENT FUNCTION HERE])
---
+++ code.js
@@ -0,0 +1,51 @@
+
+var currentDate = luxon.DateTime.local();
+var topDate = $("#currentDay");
+var printDate = currentDate.toLocaleString(luxon.DateTime.DATE_MED_WITH_WEEKDAY)
+topDate.text(printDate);
+console.log(printDate);
+
+
+function setSchedule() {
+
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+    $(".time-block").each(function () {
+        var id = $(this).attr("id");
+        var schedule = localStorage.getItem(id);
+
+        if (schedule !== null) {
+            $(this).children(".timing").val(schedule);
+        }
+    });
+}
+
+setSchedule();
+
+var saveBtn = $(".saveBtn");
+
+saveBtn.on("click", function () {
+    var time = $(this).parent().attr("id");
+    var schedule = $(this).siblings(".timing").val();
+
+    localStorage.setItem(time, schedule);
+});
+
+function pastPresentFuture() {
+    hour = time.hours();
+    $(".time-block").each(function () {
+        var thisHour = parseInt($(this).attr("id"));
+        
+        if (thisHour > hour) {
+            $(this).addClass("future")
+        }
+        else if (thisHour === hour) {
+            $(this).addClass("present");
+        }
+        else {
+            $(this).addClass("past");
+        }
+    })
+}
+
+pastPresentFuture();
